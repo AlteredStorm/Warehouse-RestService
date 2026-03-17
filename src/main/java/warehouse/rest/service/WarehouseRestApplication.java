018bdc2e98f8349d520e734ce5a22b04e24a9480
Maximilian Wadle <Maximilian.wadle@gmail.com>
2026-03-17 18:59:10 +0100

    initial commit

---
+++ src/main/java/warehouse/rest/service/WarehouseRestApplication.java
@@ -0,0 +1,13 @@
+package warehouse.rest.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseRestApplication.class, args);
+    }
+
+}
