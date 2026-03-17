79bf59ff76ed1a77223b4829d303311bb5218714
Maximilian Wadle <Maximilian.wadle@gmail.com>
2026-03-17 19:53:38 +0100

    added endpoint to get a specific location

--- src/main/java/warehouse/core/controller/LocationController.java
+++ src/main/java/warehouse/core/controller/LocationController.java
@@ -3,11 +3,9 @@ package warehouse.core.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import warehouse.core.document.Location;
+import warehouse.core.document.Product;
 import warehouse.core.service.LocationService;
 
 import java.util.List;
@@ -30,6 +28,18 @@ public class LocationController {
     @GetMapping
     public ResponseEntity<List<Location>> getLocations() {
         return new ResponseEntity<>(locationService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Location> getLocationById(@PathVariable String id) {
+        ResponseEntity<Location> responseEntity;
+        Location location = locationService.findById(id);
+        if (location == null) {
+            responseEntity = new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            responseEntity = new ResponseEntity<>(location, HttpStatus.OK);
+        }
+        return responseEntity;
     }
 
 
