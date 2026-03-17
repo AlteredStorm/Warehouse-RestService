fcf2a8887caef04112be30c45f782982a1c0a46f
Maximilian Wadle <Maximilian.wadle@gmail.com>
2026-03-17 19:58:06 +0100

    added annotations

--- src/main/java/warehouse/core/document/Location.java
+++ src/main/java/warehouse/core/document/Location.java
@@ -1,4 +1,11 @@
 package warehouse.core.document;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Location")
+@Getter
+@Setter
 public class Location {
 }
