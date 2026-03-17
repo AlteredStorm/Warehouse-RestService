3c25e540a446258c6cdccc19dc2f189520870d1d
Maximilian Wadle <Maximilian.wadle@gmail.com>
2026-03-17 19:38:48 +0100

    added RequestBody

--- src/main/java/warehouse/core/controller/ProductRestController.java
+++ src/main/java/warehouse/core/controller/ProductRestController.java
@@ -3,10 +3,7 @@ package warehouse.core.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import warehouse.core.document.Product;
 import warehouse.core.service.ProductService;
 
@@ -23,7 +20,7 @@ public class ProductRestController {
     }
 
     @PostMapping
-    public void postProduct(Product product) {
+    public void postProduct(@RequestBody Product product) {
         productService.save(product);
     }
 
